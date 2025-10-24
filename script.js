aa7006fd308031d36f04193097effc21b1cdcf9d
MattsDevLab <116913535+MattsDevLab@users.noreply.github.com>
2025-10-24 16:59:36 +0100

    Set up if statement to convert random number to rock paper scissors for computer choice funciton

--- script.js
+++ script.js
@@ -1,15 +1,37 @@
    console.log('Hellow Hellow Welcome, this is now live'); 
 
-   const rock = 0;
-   const paper = 1;
-   const scissors = 2;
+//    const rock = 0;
+//    const paper = 1;
+//    const scissors = 2;
+
+   let humanScore = 0;
+   let computerScore = 0;
+
+//    const humanSelection = getHumanChoice();
+   const computerSelection = getComputerChoice();
 
    function getComputerChoice(){
-    return Math.floor((Math.random() * 3));
+    let choice =  Math.floor((Math.random() * 3));
+
+    if(choice === 0){
+        return "rock";
+    } else if(choice === 1){
+        return "scissors";
+    } else{
+        return "paper";
+    }
    }
 
    function getHumanChoice(){
-    return prompt();
+    return prompt().toLowerCase();
    }
 
-   console.log(getHumanChoice());
+//    function playRound(humanSelection, computerSelection){
+
+//    }
+
+    console.log(getComputerChoice());
+    console.log(getHumanChoice());
+
+
+
