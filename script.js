dadf98de91d91a3f31fdfda71621e3e8001dd733
MattsDevLab <116913535+MattsDevLab@users.noreply.github.com>
2025-10-24 17:00:53 +0100

    Neaten code with each line tabbed

--- script.js
+++ script.js
@@ -1,37 +1,31 @@
-   console.log('Hellow Hellow Welcome, this is now live'); 
-
-//    const rock = 0;
-//    const paper = 1;
-//    const scissors = 2;
-
-   let humanScore = 0;
-   let computerScore = 0;
+    let humanScore = 0;
+    let computerScore = 0;
 
 //    const humanSelection = getHumanChoice();
-   const computerSelection = getComputerChoice();
+    const computerSelection = getComputerChoice();
 
-   function getComputerChoice(){
-    let choice =  Math.floor((Math.random() * 3));
+    function getComputerChoice(){
+        let choice =  Math.floor((Math.random() * 3));
 
-    if(choice === 0){
-        return "rock";
-    } else if(choice === 1){
-        return "scissors";
-    } else{
-        return "paper";
+        if(choice === 0){
+            return "rock";
+        } else if(choice === 1){
+            return "scissors";
+        } else{
+            return "paper";
+        }
     }
-   }
 
-   function getHumanChoice(){
-    return prompt().toLowerCase();
-   }
+    function getHumanChoice(){
+        return prompt().toLowerCase();
+    }
 
 //    function playRound(humanSelection, computerSelection){
 
 //    }
 
     console.log(getComputerChoice());
-    console.log(getHumanChoice());
+    console.log(getHumanRockChoice());
 
 
 
