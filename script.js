1484433eec2dda19088f003257b490205344a8ae
MattsDevLab <116913535+MattsDevLab@users.noreply.github.com>
2025-10-24 18:27:43 +0100

    Return +1 on score for human and computer player to update global variables

--- script.js
+++ script.js
@@ -23,16 +23,22 @@
     function playRound(humanChoice, computerChoice){
         if(humanChoice === "rock" && computerChoice == "scissors"){
             console.log( "You win! Rock beats Scissors");
+            return humanScore = + 1;
         } else if(humanChoice === "scissors" && computerChoice == "rock"){
             console.log( "You loose! Rock beats Scissors");
+            return computerScore = + 1;
         } else if(humanChoice === "scissors" && computerChoice === "paper"){
             console.log("You win! Scissors beats paper");
+            return humanScore = + 1;
         } else if(humanChoice === "paper" && computerChoice === "scissors"){
             console.log("You loose! Scissors beats paper");
+            return computerScore = + 1;
         } else if(humanChoice === "paper" && computerChoice === "rock"){
             console.log("You win! Paper beats rock!paper");
+             return humanScore = + 1;
         } else if(humanChoice === "rock" && computerChoice === "paper"){
             console.log("You loose! Paper beats rock!paper");
+            return computerScore = + 1;
         } else if(humanChoice === computerChoice){
             console.log("Its a draw");
         }
@@ -42,6 +48,8 @@
 
     console.log(`Human: ${humanSelection}`);
     console.log(`Computer choice: ${computerSelection}`);
+    console.log(`Human score ${humanScore}`);
+    console.log(`Computer score ${computerScore}`);
 
 
 
