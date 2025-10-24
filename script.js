6488e4caa2fe98ffd20cc660d80b45f0971a0597
MattsDevLab <116913535+MattsDevLab@users.noreply.github.com>
2025-10-24 16:26:23 +0100

    Create human choice function

--- script.js
+++ script.js
@@ -8,4 +8,8 @@
     return Math.floor((Math.random() * 3));
    }
 
-   console.log(getComputerChoice());
+   function getHumanChoice(){
+    return prompt();
+   }
+
+   console.log(getHumanChoice());
