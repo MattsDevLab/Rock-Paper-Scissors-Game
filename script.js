b941834333a5094397c829bbae502903a0d9719b
MattsDevLab <116913535+MattsDevLab@users.noreply.github.com>
2025-10-25 00:00:34 +0100

    Configured playGame function to save each of the 5 rounds in 5 variables which include getHumanChoice and getComputerChoice functions - humanSelection and computerSelection variables were removed

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-    let humanScore = 0;
-    let computerScore = 0;
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
+    // const humanSelection = getHumanChoice();
+    // const computerSelection = getComputerChoice();
 
     function getComputerChoice(){
         let choice =  Math.floor((Math.random() * 3));
@@ -19,40 +17,42 @@
         return prompt().toLowerCase();
     }
 
-    function playRound(humanChoice, computerChoice){
-        if(humanChoice === "rock" && computerChoice == "scissors"){
-            console.log( "You win! Rock beats Scissors");
-            return humanScore = + 1;
-        } else if(humanChoice === "scissors" && computerChoice == "rock"){
-            console.log( "You loose! Rock beats Scissors");
-            return computerScore = + 1;
-        } else if(humanChoice === "scissors" && computerChoice === "paper"){
-            console.log("You win! Scissors beats paper");
-            return humanScore = + 1;
-        } else if(humanChoice === "paper" && computerChoice === "scissors"){
-            console.log("You loose! Scissors beats paper");
-            return computerScore = + 1;
-        } else if(humanChoice === "paper" && computerChoice === "rock"){
-            console.log("You win! Paper beats rock!paper");
-             return humanScore = + 1;
-        } else if(humanChoice === "rock" && computerChoice === "paper"){
-            console.log("You loose! Paper beats rock!paper");
-            return computerScore = + 1;
-        } else if(humanChoice === computerChoice){
-            console.log("Its a draw");
-        }
-   }
-
     function playGame(){
-        playRound(humanSelection, computerSelection);
+        let humanScore = 0;
+        let computerScore = 0;
+
+        function playRound(humanChoice, computerChoice){
+            if(humanChoice === "rock" && computerChoice == "scissors"){
+                console.log( "You win! Rock beats Scissors");
+                return humanScore++;
+            } else if(humanChoice === "scissors" && computerChoice == "rock"){
+                console.log( "You loose! Rock beats Scissors");
+                return computerScore++;
+            } else if(humanChoice === "scissors" && computerChoice === "paper"){
+                console.log("You win! Scissors beats paper");
+                return humanScore++;
+            } else if(humanChoice === "paper" && computerChoice === "scissors"){
+                console.log("You loose! Scissors beats paper");
+                return computerScore++;
+            } else if(humanChoice === "paper" && computerChoice === "rock"){
+                console.log("You win! Paper beats rock!paper");
+                return humanScore++;
+            } else if(humanChoice === "rock" && computerChoice === "paper"){
+                console.log("You loose! Paper beats rock!paper");
+                return computerScore++;
+            } else if(humanChoice === computerChoice){
+                console.log("Its a draw");
+            }
+        }
+
+        let result1 = playRound(getHumanChoice(), getComputerChoice());
+        let result2 = playRound(getHumanChoice(), getComputerChoice());
+        let result3 = playRound(getHumanChoice(), getComputerChoice());
+        let result4 = playRound(getHumanChoice(), getComputerChoice());
+        let result5 = playRound(getHumanChoice(), getComputerChoice());
+
+        console.log(`Human score total: ${humanScore}`);
+        console.log(`Computer score total: ${computerScore}`);
     }
 
     playGame();
-
-    console.log(`Human: ${humanSelection}`);
-    console.log(`Computer choice: ${computerSelection}`);
-    console.log(`Human score ${humanScore}`);
-    console.log(`Computer score ${computerScore}`);
-
-
-
